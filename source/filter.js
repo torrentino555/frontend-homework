a8c3bbc16863868521eb31c3760234036bc237cd
Vlad Orlov <orl_vlad@mail.ru>
2017-09-19 12:13:58 +0300

    Внесены нужные правки

--- source/filter.js
+++ source/filter.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const filter = function(str) {
-    str = str.replace(new RegExp('[&"\'<>]', 'g'), function(match) {
+const filter = function(str, dictValidTags) {
+    return str.replace(new RegExp('[&"\'<>]', 'g'), function(match) {
         // Экранирование всех запрещенных символов
         const dictNotValidLiterals = new Map([
           ['&', '&amp;'],
@@ -11,41 +11,11 @@ const filter = function(str) {
           ['>', '&gt;']
         ]);
         return dictNotValidLiterals.get(match);
-    });
-    str = str.replace(
-      new RegExp('&lt;([a-zA-Z]*)&gt;|&lt;\/([a-zA-Z]*)&gt;', 'g'),
+    }).replace(
+      new RegExp('&lt;(\/?)([a-zA-Z]*)&gt;', 'g'),
       function(match, p1, p2) {
-        // Нахождение всех допустимых html-тегов и запись их в текст
-        const dictValidTags = new Map([
-            ['strong'],
-            ['em'],
-            ['b'],
-            ['br'],
-            ['h1'],
-            ['h2'],
-            ['h3'],
-            ['h4'],
-            ['h5'],
-            ['h6'],
-            ['hr'],
-            ['label'],
-            ['p'],
-            ['span'],
-            ['ul'],
-            ['ol'],
-            ['li'],
-            ['table'],
-            ['td'],
-            ['th'],
-            ['i'],
-            ['strong']
-        ]);
-        if (dictValidTags.has(p1)) {
-            return '<' + p1 + '>';
-        } else if (dictValidTags.has(p2)) {
-            return '</' + p2 + '>';
-        }
+        if (dictValidTags.indexOf(p2) != -1)
+          return '<' + p1 + p2 + '>';
         return match;
     });
-    return str;
 };
